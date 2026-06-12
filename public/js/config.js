84ba4bd359af435b5ab113dc5164e64f68048d8d
nccu108256037 <nccu108256037@gmail.com>
2026-06-12 22:57:51 +0800

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 window.SHENG_HAO_DUO_CONFIG = {
-  LIFF_ID: '2010383047-nRqwOvl6',
+  LIFF_ID: '2010383047-nRqwOvI6',
   GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbw1JL2wzHQ2jNvxumC-EGDEiXJvnK7Mj_NUX5qiuumaVfW5DivUjSkVENBaYYdYEJgI/exec'
 };
