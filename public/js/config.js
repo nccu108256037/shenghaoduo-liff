a73a8f057ea8afb769a9c79abe59d6ae5cd7888d
nccu108256037 <nccu108256037@gmail.com>
2026-07-01 23:45:22 +0800

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,14 @@
 window.SHENG_HAO_DUO_CONFIG = {
-  LIFF_ID: '2010383047-nRqwOvI6',
 
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxC2NK9ltrFX-4TYJX6mI1xKbYbIv3FgORDv1mVfIXuGX-YKIsLDYVp2dkp37wty5iB/exec'
+  // LINE LIFF
+  LIFF_ID: '2010383047-nRqwOvl6',
+
+  // 暫時保留 Apps Script（訂單、優惠券先繼續使用）
+  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxC2NK9ltrFX-4TYJX6mI1xKbYbIv3FgORDv1mVfIXuGX-YKIsLDYVp2dkp37wty5iB/exec',
+
+  // Supabase（商品先改由這裡讀）
+  SUPABASE_URL: 'https://kkflpbeouhqxatmvsuqj.supabase.co',
+
+  SUPABASE_ANON_KEY: 'sb_publishable_cXcPTCsDOdeOriHcNqxNLQ_n1bsy9xw'
+
 };
